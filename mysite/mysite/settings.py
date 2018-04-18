59559b9f7c9ecf0a0d40f1ef8b92acaafdd8efe6
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2018-04-18 13:30:24 +0200

    messages toegevoegd

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'x.x.x.x',
-        'PORT': '',
+        #'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        #'USER': '',
+        #'PASSWORD': '',
+        #'HOST': 'x.x.x.x',
+        #'PORT': '',
     }
 }
 
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Brussels'
 
 USE_I18N = True
 
--- mysite/room/views.py
+++ mysite/room/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from  django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the room index.")
+	template = loader.get_template('polls/index.html')
+	return HttpResponse(template.render(request))
