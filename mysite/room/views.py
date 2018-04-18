a40d79755730df58717785a8513557ddc4355553
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2018-04-18 14:22:11 +0200

    Probleem met index

--- mysite/room/views.py
+++ mysite/room/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from  django.template import loader
+from django.shortcuts import render
 
 def index(request):
-	template = loader.get_template('polls/index.html')
-	return HttpResponse(template.render(request))
+	return render(request, 'templates/polls/index.html')
