87426d49089ba1daf3fc6f4a41ad1cce3d68b8f0
Tom Coppens <tom.coppens@bisqit.be>
2018-04-19 11:46:10 +0200

    test
    update

--- roomsite/roomReservations/models.py
+++ roomsite/roomReservations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class RoomStatus(models.Model):
+    
+    myRoom = models.CharField(max_length=20)
+    myStatus = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.myStatus
+    
--- roomsite/roomsite/settings.py
+++ roomsite/roomsite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'roomReservations',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'roomReservations.apps.RoomreservationsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'roomsite.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'roomReservations',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '193.191.176.134',
+        'PORT': '5432',
     }
 }
 
