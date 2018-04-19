e3089145ada00abe113f0e0d6236bec8e0b6e31d
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2018-04-19 10:50:34 +0200

    nieuwe website

---
+++ roomsite/roomReservations/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RoomreservationsConfig(AppConfig):
+    name = 'roomReservations'
--- roomsite/roomsite/urls.py
+++ roomsite/roomsite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""roomsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
-    path('room/', include('room.urls')),
     path('admin/', admin.site.urls),
+    path('roomReservations/', include('roomReservations.urls')),
 ]
